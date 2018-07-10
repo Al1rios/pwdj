68dab7b2ed66984efcad51bd4f1deb489f795e7c
Al1rios <ali.rios@gmail.com>
2018-07-10 13:13:59 -0300

    Feito design da página de contatos, extraido base.html
    close#45

--- pwdj/core/tests.py
+++ pwdj/core/tests.py
@@ -3,7 +3,7 @@ import pytest
 from pwdj.django_assertions import dj_assert_contains
 
 
-def test_status_code(client):
+def test_home_status_code(client):
     response = client.get('/')
     return 200 == response.status_code
 
@@ -12,9 +12,27 @@ def test_status_code(client):
     'content', [
         'Project Work Django',
         'ali.rios@gmail.com',
-        '+55 31 971-906677',
+        '+55 21 971-906677',
+        'href="/contato/"',
     ]
 )
 def test_home(client, content):
     response = client.get('/')
+    dj_assert_contains(response, content)
+
+
+def test_contact_status_code(client):
+    response = client.get('/contato/')
+    return 200 == response.status_code
+
+
+@pytest.mark.parametrize(
+    'content', [
+        '21 971-906677',
+        'Rua Paissandu Rj',
+        '22210-085',
+    ]
+)
+def test_contact_content(client, content):
+    response = client.get('/contato/')
     dj_assert_contains(response, content)

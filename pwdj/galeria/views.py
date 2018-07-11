8a26348becc21e1f49dde43a632879b4f6aefd82
Al1rios <ali.rios@gmail.com>
2018-07-11 15:36:41 -0300

    Passado valor de modelo (objeto) Python Puro para o template
    close#54

--- pwdj/galeria/views.py
+++ pwdj/galeria/views.py
@@ -1,7 +1,24 @@
+from decimal import Decimal
+
 from django.shortcuts import render
 
 # Create your views here.
+from pwdj.galeria.models import Model
 
 
 def index(request):
-    return render(request, 'galeria/index.html')
+    ctx = {
+        'galeria':
+            [
+                Model('Social media now takes a back seat in favor of ‘new sets of tools’ to solve problems.',
+                      Decimal('400.00'),
+                      'We need people like you, and we need people not like you,” she told the audience.'),
+                Model('Social media now takes a back seat',
+                      Decimal('7000.00'),
+                      'We need people like you, and we need people not like you,” she told the audience.'),
+                Model('New sets of tools’ to solve problems.',
+                      Decimal('8600.00'),
+                      'She told the audience.')
+             ]
+    }
+    return render(request, 'galeria/index.html', context=ctx)

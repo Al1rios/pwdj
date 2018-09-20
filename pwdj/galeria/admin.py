8577a84b55797cf4b6749a85cac226791a179cbc
Al1rios <ali.rios@gmail.com>
2018-09-20 05:20:01 -0300

    Implementado
    closes #176

--- pwdj/galeria/admin.py
+++ pwdj/galeria/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
-from pwdj.galeria.models import Model
+from pwdj.galeria.models import Model, Categoria
+
+
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ('titulo',)
+    ordering = ('titulo',)
 
 
 @admin.register(Model)
 class ModelAdmin(admin.ModelAdmin):
-    list_display = 'titulo preco'.split()
-    ordering = ('titulo',)
+    list_display = 'categoria titulo preco criacao ultima_alteracao'.split()
+    ordering = ('categoria', 'titulo',)
